574778c0f55d2edc4c756c001da954cf361c38f5
eliashezron <eliashezron23@gmail.com>
2025-09-24 18:31:46 +0300

    adjust max-slippage to 1%

--- app/components/ExchangeRateDisplay.tsx
+++ app/components/ExchangeRateDisplay.tsx
@@ -11,7 +11,7 @@ export function ExchangeRateDisplay({
   fromCurrency,
   toCurrency,
   rate = "0",
-  slippage = "2.5%",
+  slippage = "1%",
 }: ExchangeRateDisplayProps) {
   return (
     <div className="mx-3 md:mx-4 mb-4 flex items-center justify-between text-sm text-neutral-400">
